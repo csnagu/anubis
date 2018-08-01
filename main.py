a62c205c614a13ed63f37fd22695e5305e8c5a52
Chikara Saito <nagulog@gmail.com>
2018-08-01 23:15:27 +0900

    --disable-gpuオプションを追加

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ url = 'http://speedwifi.home/html/login.htm'
 # Selenium settings
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver', chrome_options=options)
 
 driver.get(url)
