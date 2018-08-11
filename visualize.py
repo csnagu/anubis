768cb2b074b3d684909ea96c0a4c76c5f8cc1d66
Chikara Saito <nagulog@gmail.com>
2018-08-11 10:14:04 +0900

    日毎の通信量の観測範囲を午前2時から翌日午前1時までとする

--- visualize.py
+++ visualize.py
@@ -7,8 +7,8 @@ import plotly.graph_objs as go
 
 app = dash.Dash()
 
-traffics = list()
-date = list()
+traffics = []
+date = []
 with open("traffics.csv", "r") as f:
     for x in csv.reader(f, delimiter=','):
         traffics.append(float(x[2]))
@@ -18,23 +18,28 @@ current_date_traffic = []
 one_day_traffics = []
 one_day_traffics_date = []
 for i in range(len(date)):
-    if i == len(date)-1:
-        one_day_traffics.append(max(current_date_traffic))
-        one_day_traffics_date.append(datetime.datetime.strptime(date[i], "%Y/%m/%d %H:%M:%S").date())
-        break
-
     # 文字列から日付型へ変換
     # 年月日のみを取得する
-    current_date = datetime.datetime.strptime(date[i], "%Y/%m/%d %H:%M:%S").date()
-    next_date = datetime.datetime.strptime(date[i+1], "%Y/%m/%d %H:%M:%S").date()
+    current_date = datetime.datetime.strptime(date[i], "%Y/%m/%d %H:%M:%S")
+    if i == len(date)-1:
+        # yesterday = current_date - datetime.timedelta(days=1)
+        # one_day_traffics_date.append(yesterday.date())
+        one_day_traffics_date.append(current_date.date())
+        one_day_traffics.append(traffics[-1])
+        # one_day_traffics.append(0)
+        break
+
     # 日付のみを取得し、1日分のデータ通信量を求める
-    if current_date == next_date:
+    # 一日のスタートはAM02:00からとする（L01の仕様っぽい）
+    day_time = datetime.time(2, 0, 0, 0)
+    if current_date.hour != day_time.hour:
         current_date_traffic.append(traffics[i])
     else:
         one_day_traffics.append(max(current_date_traffic))
-        one_day_traffics_date.append(current_date)
+        # １日の通信量はAM02:00にリセットされるので日付を１日戻す
+        today = current_date - datetime.timedelta(days=1)
+        one_day_traffics_date.append(today.date())
         current_date_traffic = []
-print(one_day_traffics, '   ', one_day_traffics_date)
 
 latest_three_days_traffic = 0
 if len(one_day_traffics) >= 3:
